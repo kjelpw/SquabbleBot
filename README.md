4ed7f210cf32bdbb16ae6400d108124759bd957c
kjelpw <kjelpw@protonmail.com>
2022-03-05 14:15:49 -0700

    find a game

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 https://squabble.me/
 
 
-dict.csv is a file of 5 letter words that should be suitable for the game
-dictionary.csv is a full dictionary file, can be replaced with an updated dictionary file if need be
+dict.csv is a file of 5 letter words that should be suitable for the game. Dictionary.csv is a full dictionary file, can be replaced with an updated dictionary file if need be
--- bot.py
+++ bot.py
@@ -9,6 +9,15 @@ driver = webdriver.Firefox(options=opts)
 driver.get('https://squabble.me/')
 assert 'Squabble' in driver.title
 
-time.sleep(5)
+#start a game
+blitz_button = driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div/div[1]')
+blitz_button.click()
+time.sleep(1)
+find_game_button = driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div/div[4]')
+find_game_button.click()
+time.sleep(1)
+
+#enter anything
+
 
 driver.close()
