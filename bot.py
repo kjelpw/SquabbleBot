56e1d2ac08957639478879f1deb2a2fa3073aff5
kjelpw <kjelpw@protonmail.com>
2022-03-05 15:06:23 -0700

    able to ready up

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver import FirefoxOptions
 import time
 
@@ -17,7 +18,14 @@ find_game_button = driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/di
 find_game_button.click()
 time.sleep(1)
 
-#enter anything
+#ready up
+actions = ActionChains(driver)
+actions.send_keys('ready')
+actions.perform()
+actions.send_keys(Keys.RETURN)
+actions.perform()
 
+#start guessing words
 
+sleep(5)
 driver.close()
