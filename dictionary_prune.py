83864253623d75f4d1795cfb95f2eb1ad8e3f107
Luke Puppo <lukepuppo@gmail.com>
2022-03-06 19:59:49 -0800

    Fixed the csv writer, correctly writes the rows to the dict file.

--- dictionary_prune.py
+++ dictionary_prune.py
@@ -6,10 +6,12 @@ read_file = open('dictionary.csv', newline='')
 words = csv.reader(read_file)
 
 write_file = open('dict.csv', 'w')
-squabble_words = csv.writer(write_file, delimiter=',')
+#squabble_words = csv.writer(write_file, delimiter=',')
 
-for w in words:
-    #check word length, for spaces, and - symbol
-    if len(w[0]) == 5 and not bool(re.search(r'\s', w[0])) and not '-' in w[0]:
-        squabble_words.writerow(w[0])
-        print(w[0])
+
+with open('dict.csv', 'w', newline='') as csvfile:
+    for w in words:
+        #check word length, for spaces, and - symbol
+        if len(w[0]) == 5 and not bool(re.search(r'\s', w[0])) and not '-' in w[0]:
+            csvfile.write(w[0] + '\n')
+            print(w[0])
